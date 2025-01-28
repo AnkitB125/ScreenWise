97cfe9aef8716ef26dbc896c92fa049f42c634f1
Pascaline3263 <pascaline3263@gmail.com>
2025-01-28 22:09:07 +0300

    modified childDashboard

--- public/js/childDashboard_db.js
+++ public/js/childDashboard_db.js
@@ -1,34 +1,23 @@
 let client = require('./dbConnection');
-let collection = client.db('screenWise').collection('child');
-let { ObjectId } = require('mongodb');
+let db = client.db('screenWise');
+const collection = db.collection('timer');
 
-async function getChildData(id, callback) {
+async function getChildData(child, callback) {
     try {
-        // Validate the ID
-        if (!ObjectId.isValid(id)) {
-            return callback(null, {
-                "message": "Invalid ID format"
-            }, 400);
-        }
-
-        // Query the database to find the child by ID
-        const child = await collection.findOne({ _id: new ObjectId(id) });
-
-        if (child) {
-            return callback(null, {
-                "message": "Child data retrieved successfully",
-                "data": child,
-            }, 200);
+        const childName = child;
+        // Query the collection
+        const query = { childName: childName }; // Replace with your query
+        const childData = await collection.find(query).toArray();
+        if (childData) {
+            return callback(null, childData, 200);
         } else {
-            return callback(null, {
-                "message": "Child not found"
-            }, 404);
+            return callback(null, "Child not found", 404);
         }
     } catch (err) {
-        console.error('Error fetching child data:', err);
         return callback(err);
     }
-}
+};
+    
 
 module.exports = {
     getChildData
